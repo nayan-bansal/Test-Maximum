fb47352e5b76a9db516c5d96957b4205c30bc4f9
nayan-bansal <nayan.7991@gmail.com>
2020-10-10 02:47:14 +0530

    Added: Junit for Maximun at 1st 2nd and 3rd position for float

--- MaximunTest.java
+++ MaximunTest.java
@@ -29,6 +29,42 @@ public class MaximumTest {
 	}
 
 	@Test
+	public void testFirstPositionFloat() {
+		Float max = Maximum.Max_number_float(50.5f, 40.5f, 30.5f);
+		boolean flag;
+		if (max.equals(50.5f)) {
+			flag = true;
+		} else {
+			flag = false;
+		}
+		Assert.assertEquals(true, flag);
+	}
+
+	@Test
+	public void testSeconfPositionFloat() {
+		Float max = Maximum.Max_number_float(10.5f, 40.5f, 20.5f);
+		boolean flag;
+		if (max.equals(40.5f)) {
+			flag = true;
+		} else {
+			flag = false;
+		}
+		Assert.assertEquals(true, flag);
+	}
+	
+	@Test
+	public void testThirdPositionFloat() {
+		Float max = Maximum.Max_number_float(10.5f, 40.5f, 50.4f);
+		boolean flag;
+		if (max.equals(50.4f)) {
+			flag = true;
+		} else {
+			flag = false;
+		}
+		Assert.assertEquals(true, flag);
+	}
+	
+	@Test
 	public void testThirdPosition() {
 		Integer max = Maximum.Max_number(10, 40, 50);
 		boolean flag;
