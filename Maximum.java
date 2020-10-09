e4cd72681ac30ceb54822ab97cf8b14be809d3a2
nayan-bansal <nayan.7991@gmail.com>
2020-10-10 05:09:42 +0530

    Added: Method to take more Parameters

--- Maximum.java
+++ Maximum.java
@@ -1,33 +1,13 @@
 package Maximum;
 
+import java.util.Arrays;
 
 public class Maximum<T extends Comparable<T>> {
-	T x, y, z;
 
-	public Maximum(T x, T y, T z) {
-		this.x = x;
-		this.y = y;
-		this.z = z;
-	}
-
-	public T maximum() {
-		return Maximum.maximum(x, y, z);
-	}
-
-	// generic method to find maximum
-	public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
-		T max = x;
-		if (y.compareTo(max) > 0) {
-			max = y;
-		}
-		if (z.compareTo(max) > 0) {
-			max = z;
-		}
-		printMax(x, y, z, max);
+	// generic method to find maximum which takes any number of arguments
+	public static <T extends Comparable<T>> T maximum(T ... values) {
+		Arrays.sort(values);
+		T max=values[values.length-1];
 		return max;
-	}
-
-	public static <T> void printMax(T x, T y, T z, T max) {
-		System.out.printf("Max of %s, %s and %s is  %s\n", x, y, z, max);
 	}
 }
