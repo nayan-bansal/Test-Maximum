f1d7c4c343ceb0c1bf655370bba137ed1b0b58de
nayan-bansal <nayan.7991@gmail.com>
2020-10-10 05:15:09 +0530

    Added: Extending max method using generic

--- Maximum.java
+++ Maximum.java
@@ -2,12 +2,19 @@ package Maximum;
 
 import java.util.Arrays;
 
+//generic class
 public class Maximum<T extends Comparable<T>> {
 
 	// generic method to find maximum which takes any number of arguments
 	public static <T extends Comparable<T>> T maximum(T ... values) {
 		Arrays.sort(values);
 		T max=values[values.length-1];
+		printMax(max);
 		return max;
+	}
+	
+	public static <T> void printMax(T max) {
+		System.out.printf("Maximum :" +max);
+		System.out.println("\n");
 	}
 }
